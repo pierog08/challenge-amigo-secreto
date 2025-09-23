a53272f1e4430a75bb6b1478fbd74581d1428a32
piero <pierog@hotmail.es>
2025-09-23 12:35:49 -0500

    implementé función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let nombresAmigos = [];
+
+// Agregar un nombre al array
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+        if (nombre === "") {
+            alert("Por favor, inserte un nombre.");
+            return;
+        }
+
+        nombresAmigos.push(nombre);
+        input.value = ""; 
+        console.log(nombresAmigos);
+}
